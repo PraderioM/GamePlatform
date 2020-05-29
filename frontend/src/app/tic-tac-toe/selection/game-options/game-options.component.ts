20c232590b1ea5f1002146f3b306043c14e63f34
PraderioM <PraderioM@Gmail.com>
2020-05-29 19:31:26 +0200

    Removed unused import.

--- frontend/src/app/tic-tac-toe/selection/game-options/game-options.component.ts
+++ frontend/src/app/tic-tac-toe/selection/game-options/game-options.component.ts
@@ -1,5 +1,4 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {GameDescription} from '../../services/models';
 
 @Component({
   selector: 'app-game-options',
