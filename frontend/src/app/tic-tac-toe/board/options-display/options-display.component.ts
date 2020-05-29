9d494ef3b8f173af732d2b9ef4db494c798ffde5
PraderioM <PraderioM@Gmail.com>
2020-05-29 18:05:33 +0200

    Added instructions.

--- frontend/src/app/tic-tac-toe/board/options-display/options-display.component.ts
+++ frontend/src/app/tic-tac-toe/board/options-display/options-display.component.ts
@@ -8,10 +8,24 @@ import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 export class OptionsDisplayComponent implements OnInit {
   @Output() backToMenu = new EventEmitter<void>();
   @Input() gameId: string;
+  showInstructions = false;
+  instructions = 'Contacta a Sergi Sanchez al número 650 38 79 93';
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  toggleShowInstructions() {
+    this.showInstructions = !this.showInstructions;
+  }
+
+  getInstructionsHeader() {
+    if (this.showInstructions) {
+      return 'Hide instructions';
+    } else {
+      return 'Show instructions';
+    }
   }
 
 }
