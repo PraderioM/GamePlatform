5c5ac0748a6d4d6250d302799336403c85f6d8ea
albertobolsi <66950864+albertobolsi@users.noreply.github.com>
2020-08-08 17:14:58 +0200

    First editios of the staticmethods on Rock Paper cissors - Alberto

--- backend/games/rock-paper-scisors/main.py
+++ backend/games/rock-paper-scisors/main.py
@@ -21,11 +21,32 @@ class Player:
             self.ask_human_play(n_plays=n_plays)
 
     def ask_human_play(self, n_plays: int) -> int:
-        # Todo do stuff.
-        return 1
+        # Todo do stuff
+        for human in range(human_players):
+            n_plays = input("Please choose your play, player " + human)
+        return n_plays
+
+# class Bots:
+#     def __init__(self, name: str, is_bot: bool = True):
+#         self.name = name
+#         self.is_bot = is_bot
+#
+#     def make_play(self, n_plays: int) -> int:
+#         if self.is_bot:
+#             return randint(1, n_plays)
+#         else:
+#             self.ask_human_play(n_plays=n_plays)
+#
+#     def ask_human_play(self, n_plays: int) -> int:
+#         # Todo do stuff
+#
+#         return 1
+
+
 
 
 class Game:
+
     def __init__(self, players: List[Player], n_plays: int = 3,
                  victory_criterion: VictoryCriterion = VictoryCriterion.BY_PLAY):
         assert n_plays % 2 == 1
@@ -41,7 +62,7 @@ class Game:
         bots = cls.ask_bot_players()
         n_plays = cls.ask_n_plays()
         victory_criterion = cls.ask_victory_criterion()
-        return Game(players=human_players + bots, n_plays=n_plays, victory_criterion=victory_criterion)
+        return Game(players= human_players + bots, n_plays=n_plays, victory_criterion=victory_criterion)
 
     def play(self):
         while len(self._players) > 1:
@@ -56,18 +77,46 @@ class Game:
 
     @staticmethod
     def ask_human_players() -> List[Player]:
-        # Todo do stuff.
-        return []
+        human_players = []
+        while True:
+            try:
+                nr_human_players = int(input("Please insert number of human players: "))
+            except ValueError:
+                print("Not valid.")
+                continue
+            if nr_human_players < 0:
+                print("Number of human players cannot be negative.")
+            else:
+                break
+        for human in range(nr_human_players):
+            human_players = [*human_players, input(("Please provide name for player " + str(human + 1) + ": "))]
+
+
 
     @staticmethod
-    def ask_bot_players() -> List[Player]:
-        # Todo do stuff.
-        return []
+    def ask_bot_players(): #-> List[Bots]:
+        bots = []
+        while True:
+            try:
+                nr_bots = int(input("Please insert number of bot players: "))
+            except ValueError:
+                print("Not valid.")
+                continue
+            if nr_bots < 0:
+                print("Number of bot players cannot be negative.")
+            # if len(bots + human_players) < 2:
+            #     print("Number of bots must be at least " + str(2-nr_human_players))
+            else:
+                break
+        for bot in range(nr_bots):
+            bots = [*bots, "Bot_"+str(bot + 1)]
+
 
     @staticmethod
     def ask_n_plays() -> int:
         # Todo do stuff.
-        return 3
+        pass
+
 
     @staticmethod
     def ask_victory_criterion() -> VictoryCriterion:
@@ -81,6 +130,11 @@ class Game:
     def _get_winner_players(self, play_list: List[Tuple[Player, int]]) -> List[Player]:
         # Todo do stuff.
         return [play_list[0][0]]
+
+
+
+
+
 
 
 if __name__ == '__main__':
