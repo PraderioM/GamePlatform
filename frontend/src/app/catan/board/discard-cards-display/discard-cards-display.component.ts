ce9921b88e817c893e4bb6eb4f83d06e70cc8fd0
PraderioM <PraderioM@Gmail.com>
2020-09-03 19:31:56 +0200

    Fixed problem with deck discarding.

--- frontend/src/app/catan/board/discard-cards-display/discard-cards-display.component.ts
+++ frontend/src/app/catan/board/discard-cards-display/discard-cards-display.component.ts
@@ -13,56 +13,59 @@ export class DiscardCardsDisplayComponent implements OnInit {
   @Output() discardCards = new EventEmitter<MaterialsDeck>();
 
   materialCardsPath = assetsPath.concat('/material_cards');
-  woodImgPath = this.materialCardsPath.concat('wood.png');
-  brickImgPath = this.materialCardsPath.concat('brick.png');
-  sheepImgPath = this.materialCardsPath.concat('sheep.png');
-  wheatImgPath = this.materialCardsPath.concat('wheat.png');
-  stoneImgPath = this.materialCardsPath.concat('stone.png');
+  woodImgPath = this.materialCardsPath.concat('/wood.png');
+  brickImgPath = this.materialCardsPath.concat('/brick.png');
+  sheepImgPath = this.materialCardsPath.concat('/sheep.png');
+  wheatImgPath = this.materialCardsPath.concat('/wheat.png');
+  stoneImgPath = this.materialCardsPath.concat('/stone.png');
 
-  remainingDeck = new MaterialsDeck(this.materialsDeck.nWood, this.materialsDeck.nBrick, this.materialsDeck.nSheep,
-                                    this.materialsDeck.nWheat, this.materialsDeck.nStone);
-  discardedDeck = new MaterialsDeck();
-  toDiscardCards = Math.floor((this.materialsDeck.nMaterials + 1) / 2);
+  remainingDeck: MaterialsDeck;
+  discardedDeck: MaterialsDeck;
+  toDiscardCards: number;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.remainingDeck = new MaterialsDeck(this.materialsDeck.nWood, this.materialsDeck.nBrick, this.materialsDeck.nSheep,
+      this.materialsDeck.nWheat, this.materialsDeck.nStone);
+    this.discardedDeck = new MaterialsDeck();
+    this.toDiscardCards = Math.floor((this.materialsDeck.nMaterials + 1) / 2);
   }
 
   addToDiscardedDeck(material: string) {
     if (material === 'wood') {
-      this.materialsDeck.nWood -= 1;
+      this.remainingDeck.nWood -= 1;
       this.discardedDeck.nWood += 1;
     } else if (material === 'brick') {
-      this.materialsDeck.nBrick -= 1;
+      this.remainingDeck.nBrick -= 1;
       this.discardedDeck.nBrick += 1;
     } else if (material === 'sheep') {
-      this.materialsDeck.nSheep -= 1;
+      this.remainingDeck.nSheep -= 1;
       this.discardedDeck.nSheep += 1;
     } else if (material === 'wheat') {
-      this.materialsDeck.nWheat -= 1;
+      this.remainingDeck.nWheat -= 1;
       this.discardedDeck.nWheat += 1;
     } else if (material === 'stone') {
-      this.materialsDeck.nStone -= 1;
+      this.remainingDeck.nStone -= 1;
       this.discardedDeck.nStone += 1;
     }
   }
 
   removeFromDiscardedDeck(material: string) {
     if (material === 'wood') {
-      this.materialsDeck.nWood += 1;
+      this.remainingDeck.nWood += 1;
       this.discardedDeck.nWood -= 1;
     } else if (material === 'brick') {
-      this.materialsDeck.nBrick += 1;
+      this.remainingDeck.nBrick += 1;
       this.discardedDeck.nBrick -= 1;
     } else if (material === 'sheep') {
-      this.materialsDeck.nSheep += 1;
+      this.remainingDeck.nSheep += 1;
       this.discardedDeck.nSheep -= 1;
     } else if (material === 'wheat') {
-      this.materialsDeck.nWheat += 1;
+      this.remainingDeck.nWheat += 1;
       this.discardedDeck.nWheat -= 1;
     } else if (material === 'stone') {
-      this.materialsDeck.nStone += 1;
+      this.remainingDeck.nStone += 1;
       this.discardedDeck.nStone -= 1;
     }
   }
