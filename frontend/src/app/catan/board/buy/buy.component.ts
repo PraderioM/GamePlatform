a6bb851dd81c54ac81f9d8a01e39ec7b90df6d00
PraderioM <PraderioM@Gmail.com>
2020-09-03 18:32:04 +0200

    It is now possible to view what we have bought.

--- frontend/src/app/catan/board/buy/buy.component.ts
+++ frontend/src/app/catan/board/buy/buy.component.ts
@@ -15,73 +15,98 @@ export class BuyComponent implements OnInit {
   @Output() updateBuildingElement = new EventEmitter<string>();
   @Output() buyDevelopment = new EventEmitter<void>();
 
-  canAffordRoad: boolean;
-  canAffordSettlement: boolean;
-  canAffordCity: boolean;
-  canAffordDevelopment: boolean;
 
   dirPath = assetsPath.concat('/buy_images/');
-  roadImgPath: string;
-  settlementImgPath: string;
-  cityImgPath: string;
-  developmentImgPath: string;
 
   constructor() { }
 
   ngOnInit() {
-    this.canAffordRoad = this.materialsDeck.nWood >= 1 && this.materialsDeck.nBrick >= 1;
-    this.roadImgPath = this.canAffordRoad ? this.dirPath.concat('road.png') : this.dirPath.concat('road_blocked.png');
+  }
 
-    this.canAffordSettlement = this.materialsDeck.nWood >= 1;
-    this.canAffordSettlement = this.canAffordSettlement && this.materialsDeck.nBrick >= 1;
-    this.canAffordSettlement = this.canAffordSettlement && this.materialsDeck.nSheep >= 1;
-    this.canAffordSettlement = this.canAffordSettlement && this.materialsDeck.nWheat >= 1;
-    if (this.canAffordSettlement) {
-      this.settlementImgPath = this.dirPath.concat('settlement.png');
-    } else {
-      this.settlementImgPath = this.dirPath.concat('settlement_blocked.png');
-    }
+  canAffordRoad(): boolean {
+    return this.materialsDeck.nWood >= 1 && this.materialsDeck.nBrick >= 1;
+  }
 
-    this.canAffordCity = this.materialsDeck.nWheat >= 2 && this.materialsDeck.nStone >= 3;
-    if (this.canAffordCity) {
-      this.cityImgPath = this.dirPath.concat('city.png');
-    } else {
-      this.cityImgPath = this.dirPath.concat('city_blocked.png');
-    }
+  canAffordSettlement(): boolean {
+    let canAfford = this.materialsDeck.nWood >= 1;
+    canAfford = canAfford && this.materialsDeck.nBrick >= 1;
+    canAfford = canAfford && this.materialsDeck.nSheep >= 1;
+    canAfford = canAfford && this.materialsDeck.nWheat >= 1;
+    return canAfford;
+  }
 
-    this.canAffordDevelopment = this.materialsDeck.nSheep >= 1;
-    this.canAffordDevelopment = this.canAffordDevelopment && this.materialsDeck.nWheat >= 1;
-    this.canAffordDevelopment = this.canAffordDevelopment && this.materialsDeck.nStone >= 1;
-    this.canAffordDevelopment = this.canAffordDevelopment && this.developmentDeck.getNDevelopments() >= 1;
-    if (this.canAffordDevelopment) {
-      this.developmentImgPath = this.dirPath.concat('development.png');
+  canAffordCity(): boolean {
+    return this.materialsDeck.nWheat >= 2 && this.materialsDeck.nStone >= 3;
+  }
+
+  canAffordDevelopment(): boolean {
+    let canAfford = this.materialsDeck.nSheep >= 1;
+    canAfford = canAfford && this.materialsDeck.nWheat >= 1;
+    canAfford = canAfford && this.materialsDeck.nStone >= 1;
+    canAfford = canAfford && this.developmentDeck.getNDevelopments() >= 1;
+    return canAfford;
+  }
+
+
+  getRoadImgPath(): string {
+    if (this.canAffordRoad()) {
+      return this.dirPath.concat('road.png');
     } else {
-      this.developmentImgPath = this.dirPath.concat('development_blocked.png');
+      return this.dirPath.concat('road_blocked.png');
     }
+  }
+
+  getSettlementImgPath(): string {
+    if (this.canAffordSettlement()) {
+      return this.dirPath.concat('settlement.png');
+    } else {
+      return this.dirPath.concat('settlement_blocked.png');
+    }
+  }
+
+  getCityImgPath(): string {
+    if (this.canAffordCity()) {
+      return this.dirPath.concat('city.png');
+    } else {
+      return this.dirPath.concat('city_blocked.png');
+    }
+  }
+
+  getDevelopmentImgPath(): string {
+    if (this.canAffordDevelopment()) {
+      return this.dirPath.concat('development.png');
+    } else {
+      return this.dirPath.concat('development_blocked.png');
+    }
+  }
+
+  getNgClass(name: string): object {
+    return {logo: true, selected: this.buildingElement === name};
   }
 
   onRoadClick() {
-    if (this.canAffordRoad) {
+    console.log('can afford road ', this.canAffordRoad());
+    if (this.canAffordRoad()) {
       this.updateBuildingElement.emit('road');
     }
+    console.log(this.buildingElement);
   }
 
   onSettlementClick() {
-    if (this.canAffordSettlement) {
+    if (this.canAffordSettlement()) {
       this.updateBuildingElement.emit('settlement');
     }
   }
 
   onCityClick() {
-    if (this.canAffordCity) {
+    if (this.canAffordCity()) {
       this.updateBuildingElement.emit('city');
     }
   }
 
   onDevelopmentClick() {
-    if (this.canAffordDevelopment) {
+    if (this.canAffordDevelopment()) {
       this.buyDevelopment.emit();
     }
   }
-
 }
--- frontend/src/app/services/constants.ts
+++ frontend/src/app/services/constants.ts
@@ -1,7 +1,7 @@
 // todo change back.
 // export const backendURL = 'http://localhost:2121';
-export const backendURL = 'http://0.0.0.0:2121';
-// export const backendURL = 'http://192.168.1.19:2121';
+// export const backendURL = 'http://0.0.0.0:2121';
+export const backendURL = 'http://192.168.1.19:2121';
 // export const backendURL = 'http://85.53.252.3:27182';
 // export const backendURL = 'http://gameplatform.tetesake.site';
 
