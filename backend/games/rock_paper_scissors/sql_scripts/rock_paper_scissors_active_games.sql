4ff685c0bac4eaae86f78a30ba18571830f47768
PraderioM <PraderioM@Gmail.com>
2020-09-06 22:16:51 +0200

    Added sql scripts.

---
+++ backend/games/rock_paper_scissors/sql_scripts/rock_paper_scissors_active_games.sql
@@ -0,0 +1,13 @@
+create table rock_paper_scissors_active_games
+(
+	id uuid default uuid_generate_v4() not null,
+	creation_date time default now() not null,
+	last_updated time default now() not null,
+	player_list json,
+	current_round integer default 0 not null,
+	victory_criterion text default 'by_play'::text not null,
+	n_plays integer default 3 not null
+);
+
+alter table rock_paper_scissors_active_games owner to admin;
+
