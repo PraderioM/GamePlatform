07e6a1b0d0b977021a064c979ddb893a7eaed047
PraderioM <PraderioM@Gmail.com>
2020-09-10 18:43:04 +0200

    Forgot to remove huge update database extra.

--- backend/games/catan/endpoints/make_play.py
+++ backend/games/catan/endpoints/make_play.py
@@ -37,42 +37,6 @@ async def make_play(request: web.Request) -> web.Response:
 
             await play.update_database(db, active_games_table, database_data)
 
-            await db.execute(f"""
-                             UPDATE {active_games_table}
-                             SET current_player_index = $1,
-                                 player_list = $2,
-                                 play_list = $3,
-                                 turn_index = $4,
-                                 last_dice_result = $5,
-                                 offer = $6,
-                                 development_deck = $7,
-                                 materials_deck = $8,
-                                 knight_player = $9,
-                                 long_road_player = $10,
-                                 discard_cards = $11,
-                                 thief_moved = $12,
-                                 to_build_roads = $13,
-                                 thief_position = $14,
-                                 to_steal_players = $15
-                             WHERE id = $16
-                             """,
-                             database_data['current_player_index'],
-                             database_data['players'],
-                             database_data['plays'],
-                             database_data['turn_index'],
-                             database_data['last_dice_result'],
-                             database_data['offer'],
-                             database_data['development_deck'],
-                             database_data['materials_deck'],
-                             database_data['knight_player'],
-                             database_data['long_road_player'],
-                             database_data['discard_cards'],
-                             database_data['thief_moved'],
-                             database_data['to_build_roads'],
-                             database_data['thief_position'],
-                             database_data['to_steal_players'],
-                             database_data['id'])
-
     return await general_make_play(pool=request.app['db'], token=request.rel_url.query['token'],
                                    active_games_table=ACTIVE_GAMES_TABLE,
                                    get_game_from_database=get_game_from_database,
