ca0e4a63c3d8c590ae6f19e9897641132ea182ef
PraderioM <PraderioM@Gmail.com>
2020-09-14 11:31:27 +0200

    Added logs.

--- frontend/src/app/sudoku/services/state.service.ts
+++ frontend/src/app/sudoku/services/state.service.ts
@@ -11,20 +11,26 @@ export class StateService {
   constructor(private http: HttpClient) { }
 
   async solveSuDoKu(table: number[][], blockRows: number, blockCols: number) {
-    return await this.http
+    console.log('solving SuDoKu.');
+    const response = await this.http
       .get<FillResponse>(this.backendURL + '/solve-sudoku',
         {params: new HttpParams().set('table', table.toString())
             .set('block_rows', blockRows.toString())
             .set('block_cols', blockCols.toString())})
       .toPromise();
+    console.log('done.');
+    return response;
   }
 
   async createSuDoKu(blockRows: number, blockCols: number) {
-    return await this.http
+    console.log('creating SuDoKu.');
+    const response = await this.http
       .get<FillResponse>(this.backendURL + '/create-sudoku',
         {params: new HttpParams()
             .set('block_rows', blockRows.toString())
             .set('block_cols', blockCols.toString())})
       .toPromise();
+    console.log('done.');
+    return response;
   }
 }
