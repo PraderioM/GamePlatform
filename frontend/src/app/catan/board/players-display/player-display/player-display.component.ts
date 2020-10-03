e0a8e5003fa6113466184732c06ee84842895676
PraderioM <PraderioM@Gmail.com>
2020-10-03 13:24:15 +0100

    Now showing changes on players info.

--- frontend/src/app/catan/board/players-display/player-display/player-display.component.ts
+++ frontend/src/app/catan/board/players-display/player-display/player-display.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import {Player} from '../../../services/models';
 import {assetsPath} from '../../../services/constants';
 
@@ -7,7 +7,7 @@ import {assetsPath} from '../../../services/constants';
   templateUrl: './player-display.component.html',
   styleUrls: ['./player-display.component.css']
 })
-export class PlayerDisplayComponent implements OnInit {
+export class PlayerDisplayComponent implements OnInit, OnChanges {
   @Input() player: Player;
   @Input() hasArmy: boolean;
   @Input() hasCommercialRoute: boolean;
@@ -18,14 +18,36 @@ export class PlayerDisplayComponent implements OnInit {
   boldText: string;
   plainText: string;
 
+  previousValues = {points: 0, resources: 0, developments: 0, knights: 0};
+
   constructor() { }
 
   ngOnInit() {
     const playerName = this.player.name == null ? '' : this.player.name;
-    let showText = playerName + ' p:' + this.player.points;
-    showText = showText + ' r:' + this.player.materialsDeck.getNMaterials();
-    showText = showText + ' d:' + this.player.developmentDeck.getNDevelopments();
-    showText = showText + ' k:' + this.player.nPlayedKnights;
+
+    // Get string.
+    // points.
+    const points = this.player.points;
+    let showText = playerName + ' p:' + points;
+    showText = showText + this.getChangeString(points, this.previousValues.points);
+
+    // resources.
+    const resources = this.player.materialsDeck.getNMaterials();
+    showText = showText + ' r:' + resources;
+    showText = showText + this.getChangeString(resources, this.previousValues.resources);
+
+    // Development.
+    const developments = this.player.developmentDeck.getNDevelopments();
+    showText = showText + ' d:' + developments;
+    showText = showText + this.getChangeString(developments, this.previousValues.developments);
+
+    // Knights.
+    const knights = this.player.nPlayedKnights;
+    showText = showText + ' k:' + knights;
+    showText = showText + this.getChangeString(knights, this.previousValues.knights);
+
+
+    // Place it in bold or normal text depending on turn.
     if (this.selected) {
       this.boldText = showText;
       this.plainText = '';
@@ -34,5 +56,31 @@ export class PlayerDisplayComponent implements OnInit {
       this.plainText = showText;
     }
   }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.player.firstChange) {
+      return;
+    }
+
+    const previousPlayer: Player = changes.developmentDeck.previousValue;
+
+    // If draw differently values that have changed.
+    if (previousPlayer != null) {
+      this.previousValues.points = previousPlayer.points;
+      this.previousValues.resources = previousPlayer.materialsDeck.getNMaterials();
+      this.previousValues.developments = previousPlayer.developmentDeck.getNDevelopments();
+      this.previousValues.knights = previousPlayer.nPlayedKnights;
+    }
+  }
+
+  getChangeString(newVal: number, oldVal: number): string {
+  if (newVal > oldVal) {
+    return '(+' + (newVal - oldVal) + ')';
+} else if (newVal < oldVal) {
+    return '(-' + (oldVal - newVal) + ')';
+  } else {
+    return '';
+  }
+}
 
 }
