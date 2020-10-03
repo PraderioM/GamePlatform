591bda2cdaddb3e4ee282a22f94d71af29309500
PraderioM <PraderioM@Gmail.com>
2020-10-03 12:50:59 +0100

    Now changes in materials are signaled.

--- frontend/src/app/catan/board/materials-deck/materials-deck.component.ts
+++ frontend/src/app/catan/board/materials-deck/materials-deck.component.ts
@@ -1,5 +1,5 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {MaterialsDeck} from '../../services/models';
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {GameDescription, MaterialsDeck} from '../../services/models';
 import {assetsPath} from '../../services/constants';
 
 @Component({
@@ -7,7 +7,7 @@ import {assetsPath} from '../../services/constants';
   templateUrl: './materials-deck.component.html',
   styleUrls: ['./materials-deck.component.css', '../../../services/common.styles.css']
 })
-export class MaterialsDeckComponent implements OnInit {
+export class MaterialsDeckComponent implements OnInit, OnChanges {
   @Input() materialsDeck: MaterialsDeck;
 
   materialCardsPath = assetsPath.concat('/material_cards');
@@ -17,9 +17,39 @@ export class MaterialsDeckComponent implements OnInit {
   wheatImgPath = this.materialCardsPath.concat('/wheat.png');
   stoneImgPath = this.materialCardsPath.concat('/stone.png');
 
+  hasChanged = {wood: false, brick: false, sheep: false, wheat: false, stone: false};
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.description.firstChange) {
+      return;
+    }
+
+    const currentDeck: MaterialsDeck = changes.materialsDeck.currentValue;
+    const previousDeck: MaterialsDeck = changes.materialsDeck.previousValue;
+
+    // If draw differently values that have changed.
+    if (previousDeck != null) {
+      this.hasChanged = {
+        wood: currentDeck.nWood !== previousDeck.nWood,
+        brick: currentDeck.nBrick !== previousDeck.nBrick,
+        sheep: currentDeck.nSheep !== previousDeck.nSheep,
+        wheat: currentDeck.nWheat !== previousDeck.nWheat,
+        stone: currentDeck.nStone !== previousDeck.nStone
+      };
+    }
+  }
+
+  getNgClass(material: string) {
+    return {
+      'card-label': true,
+      tooltip: true,
+      'changed-number': this.hasChanged[material]
+    };
   }
 
 }
