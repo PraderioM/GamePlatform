7e502346577c666f684e5913153696143fe97c87
PraderioM <PraderioM@Gmail.com>
2020-11-06 23:59:03 +0000

    Reformatted.

--- frontend/src/app/rock-paper-scissors/selection/selection.component.ts
+++ frontend/src/app/rock-paper-scissors/selection/selection.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit, Output, EventEmitter, Input} from '@angular/core';
-import { GameDescription } from '../services/models';
+import {GameDescription} from '../services/models';
 import {StateService} from '../services/state.service';
 
 @Component({
