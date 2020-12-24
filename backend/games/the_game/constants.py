212e12e9a940240e4f91252ccf413e19ab6680ed
PraderioM <PraderioM@Gmail.com>
2020-12-24 09:45:02 +0000

    started backend.

---
+++ backend/games/the_game/constants.py
@@ -0,0 +1,7 @@
+ENDPOINT_SCOPE = '/the-game'
+ACTIVE_GAMES_TABLE = 'the_game_active_games'
+LEADER_BOARD_TABLE = 'the_game_leader_board'
+
+
+MAX_CARDS = 100
+ON_FIRE_CARDS = [22, 33, 44, 55, 66, 77]
---
+++ backend/games/the_game/typing_hints.py
@@ -0,0 +1,2 @@
+Card = int
+PileId = int
